fb90a9ad87bf6f3ef51c510cf27098ddd894d6ea
CedriqueCoomans <cedrique.coomans@student.kdg.be>
2025-03-29 19:44:06 +0100

    Update

--- app.py
+++ app.py
@@ -19,23 +19,33 @@ def show_films():
 
 @app.route("/films/<int:film_id>")
 def film_detail(film_id):
-    response = requests.get(f"{BASE_URL}/movies/{film_id}")
-    film = response.json() if response.status_code == 200 else None
-    return render_template("film_detail.html", film=film)
+    film_res = requests.get(f"{BASE_URL}/movies/{film_id}")
+    char_res = requests.get(f"{BASE_URL}/characters")
+
+    film = film_res.json() if film_res.status_code == 200 else None
+    characters = char_res.json() if char_res.status_code == 200 else []
+
+    film_characters = [char for char in characters if char["film_id"] == film_id]
+
+    return render_template("film_detail.html", film=film, characters=film_characters)
+
 
 
 # CHARACTERS
 @app.route("/characters")
-def show_characters():
+def all_characters():
     response = requests.get(f"{BASE_URL}/characters")
     characters = response.json() if response.status_code == 200 else []
-    return render_template("characters.html", characters=characters, title="Marvel Characters")
+    return render_template("characters.html", characters=characters)
 
-@app.route("/characters/<int:char_id>")
-def character_detail(char_id):
-    response = requests.get(f"{BASE_URL}/characters/{char_id}")
-    character = response.json() if response.status_code == 200 else None
+
+@app.route("/characters/<string:type>s/<int:char_id>")
+def character_detail(type, char_id):
+    response = requests.get(f"{BASE_URL}/characters")
+    characters = response.json() if response.status_code == 200 else []
+    character = next((char for char in characters if char["id"] == char_id and char["role"] == type), None)
     return render_template("character_detail.html", character=character)
+
 
 
 # PLANETS
